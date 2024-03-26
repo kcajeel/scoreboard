eac3561194db4f32d30a7cf5ad85470326733589
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-03-26 15:10:48 -0400

    fixed admin view of models

--- scores/models.py
+++ scores/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class teams(models.Model):
     name = models.CharField(max_length=255)
     points = models.IntegerField(default=0)
+    class Meta:
+        verbose_name_plural = 'Teams'
 
 class service_checks(models.Model):
     RESULT_CODES = {
@@ -19,12 +21,16 @@ class service_checks(models.Model):
     participant_feedback = models.TextField()
     staff_feedback = models.TextField()
     created_at = models.DateTimeField()
+    class Meta:
+        verbose_name_plural = 'Service Checks'
 
 class check_details(models.Model):
-    DETAIL_TYPES = {
+    DETAIL_TYPES = {    
         "P": "participant",
         "S": "staff"
     }
     service_check_id = models.IntegerField(),
     detail_type = models.CharField(choices = DETAIL_TYPES, max_length=11)
     detail = models.TextField()
+    class Meta:
+        verbose_name_plural = 'Check Details'
