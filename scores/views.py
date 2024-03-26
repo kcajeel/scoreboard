9a447fcc3be1df8bed3c95e5c1d7ccbe59534494
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-03-26 15:17:59 -0400

    website home now shows teams and points

--- scores/views.py
+++ scores/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from scores.models import teams
 
 # Create your views here.
 def home(request):
-    return HttpResponse("testing")
+    ordered_teams = teams.objects.order_by("-points")
+    output = ", ".join([(t.name + ', ' + str(t.points)) for t in ordered_teams])
+    return HttpResponse(output)
