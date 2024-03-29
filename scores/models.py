03a14532540168fc950566a04180f77e380efc48
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-03-29 13:39:48 -0400

    More database adjustments (last one?)

--- scores/models.py
+++ scores/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class teams(models.Model):
-    team_id = models.AutoField(primary_key=True, default=0)
-    name = models.CharField(max_length=255)
+    team_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
     points = models.IntegerField(default=0)
     class Meta:
         verbose_name_plural = 'Teams'
         db_table = 'teams'
 
 class targets(models.Model):
-    target_id = models.AutoField(primary_key=True, default=0)
+    target_id = models.AutoField(primary_key=True)
     target_host = models.CharField(max_length=255)
     team = models.ForeignKey(teams, on_delete=models.SET_NULL, null=True)
     class Meta:
@@ -26,7 +26,7 @@ class ports(models.Model):
         UNKNOWN = "UNK", "unknown"
         ERROR = "ERR", "error"
 
-    port_id = models.AutoField(primary_key=True, default=0)
+    port_id = models.AutoField(primary_key=True)
     port_number = models.CharField(max_length=255, null=True)
     service_name = models.CharField(max_length=255)
     result_code = models.CharField(choices=RESULT_CODES.choices, max_length=3, default=RESULT_CODES.UNKNOWN)
