d73ec138aac1f0339ed00b41959a194f6e2da006
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-03-29 15:03:25 -0400

    so close to working

--- scores/views.py
+++ scores/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import get_object_or_404, render
-
-from .models import teams
+from .models import teams, ports
 
 # Create your views here.
 def index(request):
     ordered_teams = teams.objects.order_by("-points")
     context = {"ordered_teams": ordered_teams}
     return render(request, "scores/index.html", context)
+    # Now I need to display the information from this query:
+    # select * from targets natural join ports
+    # where team_id = 1 -- this value can be a variable and I can do this for each team :D
     
 def detail(request, team_id):
     team = get_object_or_404(teams, pk=team_id)
-    return render(request, "scores\detail.html", {"team": team})
+    ports_for_team = ports.objects.filter(target_id=team_id).order_by("-points_obtained")
+    context = {"team": team, "ports": ports_for_team}
+    return render(request, "scores\detail.html", context)
